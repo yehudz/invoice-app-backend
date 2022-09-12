77ad842b254fe7596c3e90eea3e3b36c314afa7d
yehudz <yehuda.zeytim@gmail.com>
2022-09-12 15:56:18 -0600

    feat: Create POST request to create invoice

--- src/routes/invoices.ts
+++ src/routes/invoices.ts
@@ -1,4 +1,5 @@
 import express, {Express, Request, Response} from 'express';
+import createRandomInvoiceId from '../utils/createRandomInvoiceId';
 const Router = require('express-promise-router')
 const db = require('../../db')
 
@@ -6,14 +7,65 @@ const router = new Router()
 
 module.exports = router
 
-// Create a new board
+// Create a new invoice
 router.use(express.json())
 
-// Get all boards
+// Get all invoices
 router.get('/', async (req: Request, res: Response)=> {
   try {
     const result = await db.query('SELECT * FROM invoice')
     res.json(result.rows)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+router.post('/', async (req: Request, res: Response)=> {
+  let randomId = createRandomInvoiceId()
+  const {
+    clientname, 
+    clientemail,
+    description,
+    status,
+    paymentterms,
+    paymentdue,
+    streetaddress,
+    city,
+    postcode,
+    country
+   } = req.body;
+  const properties = 
+  `INSERT INTO invoice(
+    invoiceId, 
+    clientname,
+    clientemail,
+    description,
+    status,
+    paymentterms,
+    paymentdue,
+    streetaddress,
+    city,
+    postcode,
+    country
+  ) VALUES(
+    $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11
+  ) RETURNING *`
+  const values = [
+    randomId,
+    clientname,
+    clientemail,
+    description,
+    status,
+    paymentterms,
+    paymentdue,
+    streetaddress,
+    city,
+    postcode,
+    country
+  ]
+  try {
+    const result = await db.query(properties, values)
+    res.status(200).json(result.rows[0])
   } catch (error) {
     console.log(error)
   }
