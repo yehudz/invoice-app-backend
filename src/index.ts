caa89c60befff640f351833cf3aebc1d34b8a424
yehudz <yehuda.zeytim@gmail.com>
2022-09-12 12:16:53 -0600

    build: Implement dotenv
    Environment variables setup

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
+require('dotenv').config();
 import 
   express, 
   {
     Express, 
     Request, 
-    Response
+    Response,
   } from 'express'
 
 const app: Express = express()
-
+const PORT = process.env.PORT || 3001
 app.get('/', 
   (req: Request, res: Response)=> {
     res.send('Working on port 3001')
 })
 
-app.listen(3001, ()=> {
-  console.log("App listening on port 3001")
+app.listen(PORT, ()=> {
+  console.log(`App listening on port ${PORT}`)
 })
